4bead9b78177fced1a6a0f9d5de0e03750152104
Konstantin Barkalov <barkalov@vmk.unn.ru>
2022-03-09 14:58:52 +0300

    LU decompositions with permutation

--- SimpleLU/SimpleLU.cpp
+++ SimpleLU/SimpleLU.cpp
@@ -39,6 +39,53 @@ void LU_Decomposition(double * A, double * L, double * U, int N)
 	}
 }
 
+void LU_DecompositionP(double* A, double* L, double* U, int *p, int N)
+{
+	for (int i = 0; i < N; i++)
+	{
+		p[i] = i;
+		for (int j = 0; j < N; j++)
+		{
+			U[N * i + j] = A[N * i + j];
+		}
+	}
+	for (int i = 0; i < N; i++)
+	{
+		//поиск позиции максимального элемента в столбце
+		int pos = i;
+		for (int k = i + 1; k < N; k++)
+		{
+			if (fabs(U[p[k] * N + i]) > fabs(U[p[pos] * N + i]))
+				pos = k;
+		}
+		//перестановка
+		int tmp = p[i];
+		p[i] = p[pos];
+		p[pos] = tmp;
+
+		L[p[i] * N + i] = 1;
+		for (int k = i + 1; k < N; k++)
+		{
+			double mu = U[p[k] * N + i] / U[p[i] *N + i];
+			for (int j = i; j < N; j++)
+			{
+				U[p[k] * N + j] -= mu * U[p[i] * N + j];
+			}
+			L[p[k] * N + i] = mu;
+			L[p[i] * N + k] = 0;
+		}
+	}
+	for (int i = 1; i < N; i++)
+	{
+		for (int j = 0; j < i; j++)
+		{
+			U[p[i] * N + j] = 0;
+		}
+	}
+}
+
+
+
 void PrintResult(double * A, double * L, double * U, int N)
 {
 	//Вывод результатов
@@ -72,14 +119,61 @@ void PrintResult(double * A, double * L, double * U, int N)
 		}
 		cout<<endl;
 	}
+	cout << endl;
 }
+
+void PrintResultP(double* A, double* L, double* U,int *p, int N)
+{
+	//Вывод результатов
+	for (int i = 0; i < N; i++)
+	{
+		for (int j = 0; j < N; j++)
+		{
+			cout.width(4);
+			cout << A[i * N + j] << " ";
+		}
+		cout << endl;
+	}
+	cout << endl << "Permutation = ";
+	for (int i = 0; i < N; i++)
+	{
+		cout << p[i] << " ";
+	}
+	cout << endl<<endl;
+	
+	cout.precision(3);
+	for (int i = 0; i < N; i++)
+	{
+		for (int j = 0; j < N; j++)
+		{
+			cout.width(6);
+			cout << L[p[i] * N + j] << " ";
+		}
+		cout << endl;
+	}
+	cout << endl;
+	for (int i = 0; i < N; i++)
+	{
+		for (int j = 0; j < N; j++)
+		{
+			cout.width(6);
+			cout << U[p[i] * N + j] << " ";
+		}
+		cout << endl;
+	}
+	cout << endl;
+}
+
 
 int main(int argc, char* argv[])
 {
-	const int N = 10;
+	const int N = 20;
 	double* A = new double[N*N]; 
 	double* L = new double[N*N]; 
-	double* U = new double[N*N]; 
+	double* U = new double[N*N];
+
+	int* p = new int[N];
+
 	time_t begin, end;
 
 	//Генерация тестовой матрицы A
@@ -89,12 +183,20 @@ int main(int argc, char* argv[])
 	}
 	begin = clock();
 	//Разложение
-	LU_Decomposition(A,L,U,N);
+	//LU_Decomposition(A, L, U, N);
+	LU_DecompositionP(A,L,U,p,N);
 	end = clock();
 
-	PrintResult(A,L,U,N);
+//	PrintResult(A, L, U, N);
+
+	PrintResultP(A,L,U,p,N);
     
 	cout<<" Total time = "<< (end - begin)/1000.0<<" sec."<<endl;
+
+	delete[] A;
+	delete[] L;
+	delete[] U;
+	delete[] p;
 
 	return 0;
 }
